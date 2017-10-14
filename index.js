cf2d7b66292a2ee88362854323422cfee4a48900
Ricardo Bocchi <ricardo@mobilemind.com.br>
2017-10-14 14:51:39 -0300

    change moment to moment-mini

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var Sqlite = require( "nativescript-sqlite" );
 var dbORM = require("./orm");
-var moment = require("moment")
+var moment = require("moment-mini")
 var dbName
 var isDebug = false
 var LOAD_CACHE = {}
