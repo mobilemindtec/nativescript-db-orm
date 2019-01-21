72ed8d11ecf5f4798bb6987df091271986c5793d
Equipe Mobile Mind <ricardo@mobilemind.com.br>
2019-01-21 12:16:49 -0200

    Update base-model.js

--- base-model.js
+++ base-model.js
@@ -2,7 +2,7 @@ var BaseModel, Model, moment, orm,
   extend = function(child, parent) { for (var key in parent) { if (hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
   hasProp = {}.hasOwnProperty;
 
-orm = require("nativescript-db-orm");
+orm = require("./orm");
 
 moment = require('moment-mini');
 
